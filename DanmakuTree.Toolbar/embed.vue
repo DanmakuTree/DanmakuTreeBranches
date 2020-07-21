1c014f7a8179f0a2c4fd80eb9d12d8d41390130a
Shugen <shugen002@users.noreply.github.com>
2020-07-21 21:09:42 +0800

    先开个坑快速弹幕栏

---
+++ DanmakuTree.Toolbar/embed.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <a-button @click="openWindow">1234</a-button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data: function () {
+      return {
+        moduleId: 'ce68d480-a08b-4678-a547-65a510083410'
+      }
+    },
+    methods: {
+      openWindow () {
+        this.$module.createModuleExternalWindow(this.moduleId).then((res) => {
+          if (res.code !== 0) {
+            this.$message.error(res.msg)
+          }
+        }).catch((e) => {
+          this.$message.error(e.message || e)
+          console.error(e)
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+.container {
+  padding: 8px;
+}
+</style>
---
+++ DanmakuTree.Toolbar/externalWindow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    aa
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+body{
+  background-color: transparent;
+}
+.container{
+
+}
+</style>
