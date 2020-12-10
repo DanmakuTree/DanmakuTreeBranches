e0b04602558c535b1bb1d6dde7439fe4e14b29a6
ax4 <yaoyue718@gmail.com>
2020-12-10 20:26:18 +0800

    update 2077 "version" of DanmuViewer

---
+++ DanmakuTree.DanmuViewer-2077/dmkli.vue
@@ -0,0 +1,67 @@
+<script>
+// Reference: https://vuejs.org/v2/guide/render-function.html#v-if-and-v-for
+// props: ['items'],
+// render: function (createElement) {
+//   if (this.items.length) {
+//     return createElement('ul', this.items.map(function (item) {
+//       return createElement('li', item.name)
+//     }))
+//   } else {
+//     return createElement('p', 'No items found.')
+//   }
+// }
+
+// Reference: https://vuejs.org/v2/guide/render-function.html#Basics
+// Vue.component('anchored-heading', {
+//   render: function (createElement) {
+//     return createElement(
+//       'h' + this.level,   // tag name
+//       this.$slots.default // array of children
+//     )
+//   },
+//   props: {
+//     level: {
+//       type: Number,
+//       required: true
+//     }
+//   }
+// })
+
+// Reference: https://frontendsociety.com/why-you-shouldnt-use-vue-component-ff019fbcac2e
+// on the topic of correctly export the component
+  export default {
+    render: function (createElement) {
+      return createElement('li', this.str2dom(this.$slots.default[0].text, createElement))
+    },
+    // props: {
+    //   key: {
+    //     type: String,
+    //     required: true
+    //   }
+    // },
+    methods: {
+      str2dom (str, h) {
+        var re = /(%(username|warning)=.+?%)/g
+        return str.split(re).map((el) => {
+          switch (el) {
+          case 'username':
+            return ''
+          case 'warning':
+            return ''
+          default:
+            break
+          }
+          var m = el.match(/%(.+)=(.+)%/)
+          if (m) {
+            return h('span', {
+              attrs: { class: m[1] }
+            }, m[2])
+          } else {
+            return el
+          }
+        })
+      }
+    }
+  }
+
+</script>>
