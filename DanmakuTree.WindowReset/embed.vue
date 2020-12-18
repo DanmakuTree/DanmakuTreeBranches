ac768f34b5fb8ca09381dd63ef49c49e36a8035e
ax4 <yaoyue718@gmail.com>
2020-12-18 16:43:46 +0800

    recursive a-card for modules

--- DanmakuTree.WindowReset/embed.vue
+++ DanmakuTree.WindowReset/embed.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="container">
-    <a-card size="small" title="简单弹幕视图重置" style="width: 300px">
+    <a-card
+      v-for="e in modules"
+      size="small"
+      v-bind:key="e.id"
+      v-bind:title="e.title"
+      style="width:300px">
       <a slot="extra" href="#">more</a>
-      <a-alert v-if="!this.isInstalled('84f22a8f-61a2-4ee2-aba2-db964ed86a4f')" message="该模块未安装" banner />
-      <a-alert v-if="this.isInstalled('84f22a8f-61a2-4ee2-aba2-db964ed86a4f')" message="模块已安装" type="success" />
-      <p v-if="this.isInstalled('84f22a8f-61a2-4ee2-aba2-db964ed86a4f')" >按下面的按钮对视图进行重置</p>
-      <a-button v-if="this.isInstalled('84f22a8f-61a2-4ee2-aba2-db964ed86a4f')" @click="resetModule('84f22a8f-61a2-4ee2-aba2-db964ed86a4f')" type="danger" block>
+      <a-alert v-if="!isInstalled(e.id)" message="该模块未安装" banner />
+      <a-alert v-if="isInstalled(e.id)" message="模块已安装" type="success" />
+      <p v-if="isInstalled(e.id)" >按下面的按钮对视图进行重置</p>
+      <a-button
+        v-if="isInstalled(e.id)"
+        @click="resetModule(e.id)"
+        type="danger"
+        block>
         重置视图窗口
       </a-button>
-    </a-card>
-    <br />
-    <a-card size="small" title="[辅助]侧边弹幕栏重置" style="width: 300px">
-      <a slot="extra" href="#">more</a>
-      <a-alert v-if="!this.isInstalled('d4977bd5-ca1a-492d-ac58-2eeb90afa188')" message="该模块未安装" banner />
-      <a-alert v-if="this.isInstalled('d4977bd5-ca1a-492d-ac58-2eeb90afa188')" message="模块已安装" type="success" />
-      <p>card content</p>
-      <p>card content</p>
-      <p>card content</p>
     </a-card>
   </div>
 </template>
@@ -26,7 +26,13 @@
     data: function () {
       return {
         moduleId: '22133289-a1f7-4961-b0bb-7814714ceda0',
-        installedModuleList: []
+        installedModuleList: [],
+        modules: [
+          { id: '84f22a8f-61a2-4ee2-aba2-db964ed86a4f', title: '简单弹幕视图重置' },
+          { id: 'd4977bd5-ca1a-492d-ac58-2eeb90afa188', title: '[辅助]侧边弹幕栏重置' },
+          { id: '63d9c897-f98f-498c-9472-5f6a7910a976', title: '2077弹幕视图' } //,
+          // { id: '352b7fa9-e1a6-4630-966e-c8c7e557166d', title: '弹幕语音姬' }
+        ]
       }
     },
     mounted () {
